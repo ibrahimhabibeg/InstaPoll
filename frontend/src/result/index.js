1ac0a08e83c1fda2549db8f97fe9aaa1b6883796
Ibrahim Habib <ibrahimhabib.eg@gmail.com>
2024-11-28 11:32:16 +0200

    Add result page with chart display and socket integration for live updates

---
+++ frontend/src/result/index.js
@@ -0,0 +1,36 @@
+Chart.defaults.font.size = 16;
+Chart.defaults.color = "#f3f4f6";
+document.addEventListener("DOMContentLoaded", () => {
+  const code = new URLSearchParams(window.location.search).get("code");
+
+  const chart = new Chart(document.getElementById("chart"), {
+    type: "bar",
+    data: {
+      labels: [],
+      datasets: [
+        {
+          label: "# of Votes",
+          data: [],
+          backgroundColor: "rgba(132, 204, 22, 0.5)",
+          hoverBackgroundColor: "rgba(132, 204, 22, 0.8)",
+          borderColor: "rgba(132, 204, 22, 1)",
+          borderWidth: 1,
+          borderRadius: 10,
+          indexAxis: "y",
+          maxBarThickness: 75,
+        },
+      ],
+    },
+  });
+
+  const socket = io(`http://localhost:3000?code=${code}`);
+
+  socket.on("poll", (pollData) => {
+    let { question, options } = pollData;
+    document.getElementById("question").innerText = question;
+    options = options.sort((a, b) => b.votes - a.votes);
+    chart.data.labels = options.map((option) => option.text);
+    chart.data.datasets[0].data = options.map((option) => option.votes);
+    chart.update();
+  });
+});
