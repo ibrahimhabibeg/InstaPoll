53de87a3252ef73fbba534148b589846fa0ecc9b
Ibrahim Habib <ibrahimhabib.eg@gmail.com>
2024-11-29 16:26:06 +0200

    Refactor navigation links to use anchor tags for better user feedback

--- frontend/src/nav.js
+++ frontend/src/nav.js
@@ -1,20 +1,20 @@
-const navLinks = document.getElementById("nav-links");
-const navHamburger = document.getElementById("nav-hamburger");
+document.addEventListener("DOMContentLoaded", async function () {
+  const navLinks = document.getElementById("nav-links");
+  const navHamburger = document.getElementById("nav-hamburger");
 
-navHamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("show");
+  navHamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+  });
+
+  const code = localStorage.getItem("code");
+  const token = localStorage.getItem("token");
+  if (!code || !token) {
+    document.getElementById("nav-edit").style.display = "none";
+  }
+  if (!code) {
+    document.getElementById("nav-results").style.display = "none";
+    document.getElementById("nav-share").style.display = "none";
+  } else {
+    document.getElementById("nav-results").href = `/result?code=${code}`;
+  }
 });
-
-const code = localStorage.getItem("code");
-const token = localStorage.getItem("token");
-if (!code || !token) {
-  document.getElementById("nav-edit").style.display = "none";
-}
-if (!code) {
-  document.getElementById("nav-results").style.display = "none";
-  document.getElementById("nav-share").style.display = "none";
-} else {
-  document
-    .getElementById("nav-results")
-    .querySelector("a").href = `/result?code=${code}`;
-}
