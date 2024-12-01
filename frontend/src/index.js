edd37b781d734ac83a3558e5bfa079802aef1fc8
Ibrahim Habib <ibrahimhabib.eg@gmail.com>
2024-12-01 21:29:36 +0200

    Make the home page

---
+++ frontend/src/index.js
@@ -0,0 +1,6 @@
+document.getElementById("Join").addEventListener("click", function(){
+  window.location.href = "/join/";
+});
+document.getElementById("Create").addEventListener("click", function(){
+  window.location.href = "/create/";
+});
